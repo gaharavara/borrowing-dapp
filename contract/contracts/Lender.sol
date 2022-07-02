ac9fbce7696c19dc49177902cead8e77ff65ba2b
gaharavara <abhishek.singh@nova-benefits.com>
2022-07-02 18:27:53 +0530

    Finalize the borrow and getCollateral methods.

--- contract/contracts/Lender.sol
+++ contract/contracts/Lender.sol
@@ -23,21 +23,37 @@ contract Lender {
   LenderToken public LEND;
   GetEthPrice private EthPrice;
 
+    event Deposit(uint256 collateralDeposited, uint256 amountMinted);
+    event Withdraw(uint256 collateralWithdrawn, uint256 amountBurned);
+
   // the amount of eth lender contract has
   uint collateral;
   // the amount of LEND tokens minted / in supply
-  uint mintedLend;
+  uint mintedLEND;
 
-  constructor(string memory) {
+  function getLatestPrice() public view returns (uint256) {
+    uint price = uint(EthPrice.getLatestPrice());
+    return price(10**10);
   }
 
   function borrow(uint collateralAmount) public payable {
     // Here we will take the collateral and submit 50%
     // of equivalent amount of token to the borrower's account
+    uint256 amountToMint = collateralAmount * getLatestPrice();
+    token.mint(msg.sender, amountToMint);
+    collateral += collateralAmount;
+    mintedLEND += amountToMint;
+    emit Deposit(amountToDeposi, amountToMint);
   }
 
   function getCollateral(uint tokenAmount) public payable {
     // Here we will return the collateral to the borrowers address
+    uint256 amountOfCollateralToWithdraw = tokenAmount / getEthUSDPrice();
+    token.burn(msg.sender, tokenAmount);
+    collateral -= amountOfCollateralToWithdraw;
+    mintedLEND -= tokenAmount;
+    payable(msg.sender).transfer(tokenAmount);
+    emit Withdraw(tokenAmount, amountOfCollateralToWithdraw);
   }
 
 }
